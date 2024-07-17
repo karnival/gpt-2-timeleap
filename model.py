f0acf2ba8b091aa42fb47c0177b3c04293e31494
Tamay Besiroglu <52602689+Besiroglu@users.noreply.github.com>
2024-07-17 19:36:18 -0400

    model.py

--- model.py
+++ model.py
@@ -80,8 +80,8 @@ class CausalSelfAttention(nn.Module):
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
-        self.c_proj  = nn.Linear(2 * config.n_embd, config.n_embd, bias=config.bias)
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)  # Updated projection
         self.dropout = nn.Dropout(config.dropout)
         self.activation = config.activation
 

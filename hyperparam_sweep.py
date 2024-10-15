6f276034fb16c09bdde9d0839c7a42bde55ffe5c
Pablo Villalobos <pablo@epochai.org>
2024-10-15 14:21:06 +0200

    Sweep changes

--- hyperparam_sweep.py
+++ hyperparam_sweep.py
@@ -1,46 +1,52 @@
-
-
-
-
-
 models = {
     5: {
     "n_layer": 6,
     "n_head": 4,
     "n_embd": 224,
     "vocab_size": 2000,
-    "max_tokens": 5e6*20*4
+    "max_tokens": 5e6*20*4 // (1024*256) * (1024*256),
+    "dataset": "fineweb2",
+    "d_files": 2,
         },
     16: {
     "n_layer": 12,
     "n_head": 5,
     "n_embd": 320,
     "vocab_size": 5000,
-    "max_tokens": 16e6*20*4
+    "max_tokens": 16e6*20*4 // (1024*256) * (1024*256),
+    "dataset": "fineweb5",
+    "d_files": 2,
         },
     47: {
     "n_layer": 18,
     "n_head": 7,
     "n_embd": 448,
     "vocab_size": 9000,
-    "max_tokens": 47e6*20*4
+    "max_tokens": 47e6*20*4 // (1024*256) * (1024*256),
+    "dataset": "fineweb9",
+    "d_files": 5,
         }
 }
 
-batch_sizes = [32, 64, 128, 256]
-beta2 = [0.95, 0.99]
-lrs = [3e-4, 6e-4, 1.2e-3, 2.4e-3, 4.8e-3, 1e-2]
+batch_sizes = [16, 32, 64, 128]
+beta2 = [0.95]
+lrs = [6e-4, 1.2e-3, 2.4e-3, 4.8e-3, 1e-2]
 
-for n, params in models.items():
+ga = 1
+
+for n, params in [(k, models[k]) for k in [47,16,5]]:
     i = 0
     for bs in batch_sizes:
+        if n == 47 and bs > 64:
+            ga = bs // 64
+            bs = 64
         for b2 in beta2:
             for lr in lrs:
-                n_iters = int(params['max_tokens']) // (1024*bs)
-                eval_interval = n_iters // 50
+                n_iters = int(params['max_tokens']) // (1024*bs*ga)
+                eval_interval = n_iters
                 warmup = n_iters // 80
                 config = f"""
-out_dir = 'out-s{n}M_v{params['vocab_size']}_d{params['n_embd']}_l{params['n_layer']}_lin_bs{bs}_wm5p_lr{lr}_b2{b2}'
+out_dir = 'out-s{n}M_v{params['vocab_size']}_d{params['n_embd']}_l{params['n_layer']}_lin_bs{bs*ga}_wm5p_lr{lr}_b2{b2}'
 eval_interval = {eval_interval} # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -49,10 +55,11 @@ always_save_checkpoint = True
 
 wandb_log = True # override via command line if you like
 wandb_project = 'scaling_laws'
-wandb_run_name = 's{n}M_v{params['vocab_size']}_d{params['n_embd']}_l{params['n_layer']}_lin_bs{bs}_wm5p_lr{lr}_b2{b2}'
+wandb_run_name = 'sweep2_s{n}M_v{params['vocab_size']}_d{params['n_embd']}_l{params['n_layer']}_lin_bs{bs*ga}_wm5p_lr{lr}_b2{b2}'
 
-dataset = 'fineweb'
-gradient_accumulation_steps = 1
+dataset = "{params['dataset']}"
+data_files = {params['d_files']}
+gradient_accumulation_steps = {ga}
 batch_size = {bs}
 block_size = 1024
 
@@ -79,8 +86,6 @@ weight_decay = 1e-4/learning_rate
 
                 with open(f"config{i}.py", "w") as f:
                     f.write(config)
-                with open(f"script{i}.py", "w") as f:
-                    f.write(f"python train.py config{i}.py\n")
                 i += 1
 
 
@@ -95,7 +100,7 @@ import time
 MAX_GPUS = 8
 
 def run_script(script, gpu_id):
-    command = f"CUDA_VISIBLE_DEVICES={gpu_id} python {script}"
+    command = f"CUDA_VISIBLE_DEVICES={gpu_id} python3 {script}"
     process = subprocess.Popen(command, shell=True)
     process.wait()
 
@@ -109,6 +114,7 @@ def worker(task_queue, gpu_semaphore):
             print(f"Running {script} on GPU {gpu_id}")
             run_script(script, gpu_id)
             print(f"Finished {script} on GPU {gpu_id}")
+            gpu_semaphore.release_gpu(gpu_id)
         task_queue.task_done()
 
 class GPUSemaphore:
@@ -159,5 +165,5 @@ def main(scripts):
 
 if __name__ == "__main__":
     # Example usage
-    scripts = [f"script_{i}.py" for i in range(6*4*2*3)]  # List of scripts to run
+    scripts = [f"train.py config{i}.py" for i in range(4*5*3)]  # List of scripts to run
     main(scripts)

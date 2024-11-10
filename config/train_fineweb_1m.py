3d4259de9789e845bf70436e2d24eca22d08e456
Pablo Villalobos <pablo@epochai.org>
2024-11-10 14:13:18 +0100

    configs

--- config/train_fineweb_1m.py
+++ config/train_fineweb_1m.py
@@ -1,8 +1,8 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-s0.7M_v1168_d128_4_const_bs8_wm5p_lr8e3'
-eval_interval = 1000 # keep frequent because we'll overfit
+out_dir = 'out-fw_s1M_v1168_d128_4_const_bs8_wm5p_lr8e3'
+eval_interval = 1142 # keep frequent because we'll overfit
 eval_iters = 20
 log_interval = 10 # don't print too too often
 
@@ -11,7 +11,7 @@ always_save_checkpoint = True
 
 wandb_log = True # override via command line if you like
 wandb_project = 'scaling_laws'
-wandb_run_name = 's0.7M_v1168_d128_4_const_bs8_wm5p_lr8e3'
+wandb_run_name = 'fw_s1M_v1168_d128_4_const_bs8_wm5p_lr8e3'
 log_activations = True
 
 dataset = 'fineweb'
@@ -20,7 +20,7 @@ gradient_accumulation_steps = 1
 batch_size = 8
 block_size = 2048
 
-vocab_size = 2000
+vocab_size = 1168
 
 # baby GPT model :)
 n_layer = 4
@@ -28,14 +28,14 @@ n_head = 2
 n_embd = 128
 dropout = 0
 
-max_iters = 1000
+max_iters = 1142
 learning_rate = 8e-3
-decay_lr = False
-#lr_decay='linear'
-#lr_decay_iters = 1000 # make equal to max_iters usually
+#decay_lr = False
+lr_decay='linear'
+lr_decay_iters = 1142 # make equal to max_iters usually
 min_lr = 8e-4
 
-warmup_iters = 50
+warmup_iters = 57
 
 weight_decay = 1e-4/learning_rate
 z_loss = 1e-4
--- config/train_fineweb_3m.py
+++ config/train_fineweb_3m.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-fw_s3M_v1168_d128_4_const_bs8_wm5p_lr8e3'
+out_dir = 'out-fw_s3M_v1539_d192_6_const_bs8_wm5p_lr8e3'
 eval_interval = 3600 # keep frequent because we'll overfit
 eval_iters = 20
 log_interval = 10 # don't print too too often
@@ -11,7 +11,7 @@ always_save_checkpoint = True
 
 wandb_log = True # override via command line if you like
 wandb_project = 'scaling_laws'
-wandb_run_name = 'fw_s3M_v1168_d128_4_const_bs8_wm5p_lr8e3'
+wandb_run_name = 'fw_s3M_v1539_d192_6_const_bs8_wm5p_lr8e3'
 log_activations = True
 
 dataset = 'fineweb'

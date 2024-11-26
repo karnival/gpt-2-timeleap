5b3a6e8fe147a804bd3c773d4d359a2539ec8361
Pablo Villalobos <pablo@epochai.org>
2024-11-26 15:56:03 +0100

    Anneal multiple sizes

--- train_anneal.py
+++ train_anneal.py
@@ -2,40 +2,68 @@ import os
 
 dataset = 'fineweb'
 
-n = 1
-beta2 = [0.95]
-lr = 5e-3
-bs = 8
+version = 1
+
+n = 3
 b2 = 0.95
 ga = 1
 max_overtrain = 8
 
-model = {
+models =  {
+    1: {
     "n_layer": 4,
     "n_head": 2,
     "n_embd": 128,
     "vocab_size": 1168,
-    "max_tokens": ((12*4*128**2 + 128*1168)*20*max_overtrain // (2048*bs*ga)) * (2048*bs*ga),
+    "max_tokens": ((12*4*128**2 + 128*1168)*20*max_overtrain // (2048*8)) * (2048*8),
     "dataset": f"{dataset}_1168",
     "d_files": 1,
-        }
+    "batch_size": 8,
+    "learning_rate": 5e-3
+        },
+    3: {
+    "n_layer": 6,
+    "n_head": 3,
+    "n_embd": 192,
+    "vocab_size": 1539,
+    "max_tokens": ((12*6*192*2 + 192*1539)*20*max_overtrain // (2048*16)) * (2048*16),
+    "dataset": f"{dataset}_1539",
+    "d_files": 1,
+    "batch_size": 16,
+    "learning_rate": 2e-3
+        },
+    10: {
+    "n_layer": 12,
+    "n_head": 4,
+    "n_embd": 256,
+    "vocab_size": 1871,
+    "max_tokens": ((12*12*256*2 + 256*1871)*20*max_overtrain // (2048*32)) * (2048*32),
+    "dataset": f"{dataset}_1871",
+    "d_files": 1,
+    "batch_size": 32,
+    "learning_rate": 2e-3,
+        },
+}
 
+model = models[n]
 
+bs = model['batch_size']
+lr = model['learning_rate']
 n_iters = int(model['max_tokens']) // (2048*bs*ga)
 eval_interval = n_iters//(10*max_overtrain//4)
 warmup = n_iters // (20*max_overtrain)
 
 for i in range(0, max_overtrain*20, 8):
     if i == 0:
-        name = 'backbone1'
+        name = f'backbone_{version}'
         init_from = 'scratch_0'
         max_iters = n_iters
         evint = eval_interval
         decay_lr = False
     else:
-        name = f'm{str(i)}'
+        name = f'm{str(i)}_{version}'
         init_from = 'resume_0'
-        max_iters = eval_interval * i // 8
+        max_iters = int(eval_interval * i // 8 / 0.8)
         evint = max_iters
         decay_lr = True
 
